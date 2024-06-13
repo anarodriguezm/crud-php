7839702f7c0d4d5115a685e1d383aed25c07ff1e
anarodriguezm <anamrm1999@gmail.com>
2024-06-13 13:24:12 +0200

    cambios

--- sql/database.sql
+++ sql/database.sql
@@ -1,16 +1,16 @@
-CREATE TABLE users (
+CREATE TABLE cats (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  surname VARCHAR(100) NOT NULL,
   name VARCHAR(100) NOT NULL,
-  age INT UNSIGNED NOT NULL
+  breed VARCHAR(100) NOT NULL,
+  age INT UNSIGNED NOT NULL,
+  owner_id INT UNSIGNED,
+  FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO users (surname, name, age) VALUES('Casas', 'Julian', 27);
-INSERT INTO users (surname, name, age) VALUES('Oro', 'Carlos', 32);
-INSERT INTO users (surname, name, age) VALUES('Valencia', 'Pablo', 29);
-INSERT INTO users (surname, name, age) VALUES('Dimas', 'Maria', 19);
-INSERT INTO users (surname, name, age) VALUES('Corrales', 'Marcos', 26);
-INSERT INTO users (surname, name, age) VALUES('Perez', 'Juan', 21);
-INSERT INTO users (surname, name, age) VALUES('Zapata', 'Pedro', 23);
-
-
+INSERT INTO cats (name, breed, age, owner_id) VALUES('Mimi', 'Siamese', 3, 1);
+INSERT INTO cats (name, breed, age, owner_id) VALUES('Luna', 'Persian', 2, 2);
+INSERT INTO cats (name, breed, age, owner_id) VALUES('Simba', 'Maine Coon', 4, 3);
+INSERT INTO cats (name, breed, age, owner_id) VALUES('Nina', 'Sphynx', 1, 4);
+INSERT INTO cats (name, breed, age, owner_id) VALUES('Toby', 'Bengal', 5, 5);
+INSERT INTO cats (name, breed, age, owner_id) VALUES('Leo', 'British Shorthair', 2, 6);
+INSERT INTO cats (name, breed, age, owner_id) VALUES('Coco', 'Russian Blue', 3, 7);
